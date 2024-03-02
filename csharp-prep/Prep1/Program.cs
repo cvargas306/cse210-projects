18bf5cd353797924a0107f754106705bfa6fe721
cvargas306 <cavbcr@gmail.com>
2024-03-01 23:56:58 -0600

    Prep 2 completed

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,16 +1,52 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("What is your first name? ");
-        string name = Console.ReadLine();
+        Console.WriteLine("What is your grade percentage? ");
+        string percentage = Console.ReadLine();
+        int percent = int.Parse(percentage); 
 
-        Console.WriteLine("What is your last name? ");
-        string lastname = Console.ReadLine();
-        
-        Console.WriteLine($"Your name is {lastname}, {name} {lastname}");
+        string grade = "";
+
+        if (percent >= 90)
+        {
+            grade = "A";
+        }
+
+        else if (percent >= 80)
+        {
+            grade = "B";
+        }
+
+        else if (percent >= 70)
+        {
+            grade = "C";
+        }
+
+        else if (percent >= 60)
+        {
+            grade = "D";
+        }
+
+        else 
+        {
+            grade = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {grade}");
+
+        if (percent >= 70)
+        {
+            Console.WriteLine("You approved.");
+        }
+
+        else
+        {
+            Console.WriteLine("Keep trying");
+        }
     }
 }
 
