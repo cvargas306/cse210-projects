63f59c2bc06b42df98bc426dea68d462b2514f9b
cvargas306 <cavbcr@gmail.com>
2024-03-02 00:05:18 -0600

    Prep 1 completed

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,48 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage? ");
+        string percentage = Console.ReadLine();
+        int percent = int.Parse(percentage); 
+
+        string grade = "";
+
+        if (percent >= 90)
+        {
+            grade = "A";
+        }
+
+        else if (percent >= 80)
+        {
+            grade = "B";
+        }
+
+        else if (percent >= 70)
+        {
+            grade = "C";
+        }
+
+        else if (percent >= 60)
+        {
+            grade = "D";
+        }
+
+        else 
+        {
+            grade = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {grade}");
+
+        if (percent >= 70)
+        {
+            Console.WriteLine("You approved.");
+        }
+
+        else
+        {
+            Console.WriteLine("Keep trying");
+        }
     }
 }
+
