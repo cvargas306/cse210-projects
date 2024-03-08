b006ddd4f55dedbe6d32649dea2fb43dac5e3450
cvargas306 <cavbcr@gmail.com>
2024-03-07 22:15:17 -0600

    Learning 02 completed

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "BYU";
+        job1._startYear = 2018;
+        job1._endYear = 2022;
+
+        Job job2 = new Job();
+        job1._jobTitle = "Manager";
+        job1._company = "Intel";
+        job1._startYear = 2020;
+        job1._endYear = 2024;
+
+        Resume myResume = new Resume();
+        myResume._name = "Carlos Vargas";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
+
     }
 }
