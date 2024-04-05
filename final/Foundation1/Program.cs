9297511c2b000e899738c3fe03f6dc95a7cb8e73
cvargas306 <cavbcr@gmail.com>
2024-04-05 17:29:21 -0600

    Program 1 corrections

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 public class Comment
 {
-    public string CommneterName {get; set;}
+    public string CommenterName {get; set;}
     public string Text {get; set;}
 }
 
@@ -18,4 +18,34 @@ public class Video
     {
         return Comments.Count;
     }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<Video> videos=new List<Video>
+        {
+            new Video
+            {
+                Title= "Video 1",
+                Author= "Author 1",
+                Length= 120,
+                Comments= new List<Comment>
+                {
+                    new Comment { CommenterName = "User 1", Text = "Great video!" },
+                    new Comment { CommenterName = "User 2", Text = "Thanks for sharing!" }
+                }
+            },
+        };
+        foreach (var video in videos)
+        {
+            Console.WriteLine($"Title: {video.Title}, Author: {video.Author}, Length: {video.Length}, Number of comments: {video.GetNumbersOfComments()}");
+            foreach (var comment in video.Comments)
+            {
+                Console.WriteLine($" Commnet by {comment.CommenterName}:, {comment.Text} ");
+            }
+        }
+    }
+
 }
