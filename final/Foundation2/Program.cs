7263ccba1b5d7dce19c714ca31de57ce52adb007
cvargas306 <cavbcr@gmail.com>
2024-04-06 20:29:31 -0600

    Products added

--- final/Foundation2/Program.cs
+++ final/Foundation2/Program.cs
@@ -65,3 +65,79 @@ public class Order
         return $"{Customer.Name}\n{Customer.Address}";
     }
 }
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var orders = new List<Order>
+        {
+            new Order
+            {
+                Customer = new Customer
+                {
+                    Name = "Lisa Simpson",
+                    Address = new Address
+                    {
+                        Street = "712 Red Bark Lane",
+                        City = "Henderson",
+                        State = "Nevada",
+                        Country = "USA"
+                    }
+                },
+                Products = new List<Product>
+                {
+                    new Product { Name = "Widget", ProductId = 1, Price = 18.99m, Quantity = 3 },
+                    new Product { Name = "Gadget", ProductId = 2, Price = 28.99m, Quantity = 2 },
+                    new Product { Name = "Laptop", ProductId = 3, Price = 385.80m, Quantity = 1 }
+                }
+            },
+
+            new Order
+            {
+                Customer = new Customer
+                {
+                    Name = "Walter White",
+                    Address = new Address
+                    {
+                        Street = "Negra Arroyo Lane",
+                        City = "Albuquerque",
+                        State = "New Mexico",
+                        Country = "USA"
+                    }
+                },
+                Products = new List<Product>
+                {
+                    new Product { Name = "Monitor", ProductId = 1, Price = 75.20m, Quantity = 2 },
+                    new Product { Name = "CPU", ProductId = 2, Price = 98.50m, Quantity = 1 }
+                }
+            },
+
+            new Order
+            {
+                Customer = new Customer
+                {
+                    Name = "Light Yagami",
+                    Address = new Address
+                    {
+                        Street = "5-2-1, Chuo",
+                        City = "Ginza Region",
+                        State = "Tokyo",
+                        Country = "JPN"
+                    }
+                },
+                Products = new List<Product>
+                {
+                    new Product { Name = "Notebook", ProductId = 1, Price = 11.75m, Quantity = 1 },
+                    new Product { Name = "Pen", ProductId = 2, Price = 3.65m, Quantity = 3 }
+                }
+            }
+            
+        };
+
+        foreach (var order in orders)
+        {
+            Console.WriteLine($"Order for {order.Customer.Name}:\nTotal Cost: ${order.TotalCost()}\nPacking Label:\n{order.PackingLabel()}\nShipping Label:\n{order.ShippingLabel()}\n");
+        }
+    }
+}
