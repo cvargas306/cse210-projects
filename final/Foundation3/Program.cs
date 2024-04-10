75788e144ec2403ae2987387e4f99130cbe80ef3
cvargas306 <cavbcr@gmail.com>
2024-04-09 22:17:39 -0600

    class Address and class Event generated

--- final/Foundation3/Program.cs
+++ final/Foundation3/Program.cs
@@ -1,9 +1,28 @@
 using System;
+using System.Collections.Generic;
 
-class Program
+public class Address
 {
-    static void Main(string[] args)
+    public string Street {get; set;}
+    public string City {get; set;}
+    public string State {get; set;}
+    public string Country {get; set;}
+}
+
+public abstract class Event
+{
+    public string Title {get; set;}
+    public string Description {get; set;}
+    public DateTime Date {get; set;}
+    public TimeSpan Time {get; set;}
+    public Address Address {get; set;}
+
+    public abstract string GetFullDetails();
+    public abstract string GetShortDescription();
+
+    public string GetStandardDetails()
     {
-        Console.WriteLine("Hello Foundation3 World!");
+        return $"Title: {Title}\nDescription: {Description}\nDate: {Date}\nTime: {Time}\nAddress: {Address.Street}, {Address.City}, {Address.State}, {Address.Country}";
     }
 }
+
